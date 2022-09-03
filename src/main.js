f94eaa5f931fdd4346c92cf7df71e28fe99391df
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2022-09-03 17:23:50 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/assets/css/costume.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import App from './App.svelte';
 
 const app = new App({
